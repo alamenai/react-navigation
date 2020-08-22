7d02e43e2c2ac0de1090ecb7b7c0039d4a72e3f4
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 13:49:40 -0700

    :zap: Add mr classes for mobile devices

--- src/components/features/Navigation/Linker.js
+++ src/components/features/Navigation/Linker.js
@@ -4,12 +4,12 @@ import { NavLink } from "react-router-dom"
 import FeatureLink from "./Links/Features"
 
 const Linker = () => (
-    <nav >
-        <ul className="flex text-white font-helvetica tracking-wide text-base">
+    <nav>
+        <ul className="flex text-white font-helvetica tracking-wide text-base xs:text-xs sm:text-sm md:text-sm justify-between xs:px-6">
             <FeatureLink />
-            <li className="mr-8"><NavLink to={ROUTES.PRODUCTS.link} activeClassName="cursor-pointer text-orange-400"> {ROUTES.PRODUCTS.name}</NavLink></li>
-            <li className="mr-8"><NavLink to={ROUTES.CUSTOMERS.link} activeClassName="cursor-pointer text-orange-400" >{ROUTES.CUSTOMERS.name}</NavLink></li>
-            <li className="mr-8"><NavLink to={ROUTES.SALES.link} activeClassName="cursor-pointer text-orange-400">{ROUTES.SALES.name}</NavLink></li>
+            <li className="xl:mr-6 sm:mr-8 md:mr-8 mr-4"><NavLink to={ROUTES.PRODUCTS.link} activeClassName="cursor-pointer text-orange-400"> {ROUTES.PRODUCTS.name}</NavLink></li>
+            <li className="xl:mr-6 sm:mr-8 md:mr-8 mr-4"><NavLink to={ROUTES.CUSTOMERS.link} activeClassName="cursor-pointer text-orange-400" >{ROUTES.CUSTOMERS.name}</NavLink></li>
+            <li className="xl:mr-6 sm:mr-8 md:mr-8 mr-4"><NavLink to={ROUTES.SALES.link} activeClassName="cursor-pointer text-orange-400">{ROUTES.SALES.name}</NavLink></li>
         </ul>
     </nav>
 )
