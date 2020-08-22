31e7a3aa2f83c22d80523730e574feb7e9a9ac7e
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 05:33:12 -0700

    :tada: Create Search Component

---
+++ src/components/features/Search/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Search = () => (
+    <input
+        className=" focus:shadow-outline-orange font-helvetica outline-none h-10 rounded-full ml-5 mr-20 px-5 font-medium placeholder-gray-400 tracking-wide xl:w-1/4"
+        type="text"
+        placeholder="Search timber"
+    />
+)
+export default Search
