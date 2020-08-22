10139c34e198948ee35c26991232bc7a7e41b361
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 18:10:22 -0700

    :tada: Create Textfield Component

---
+++ src/components/library/Textfield/Outline.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react"
+
+const Field = ({
+    type,
+    name,
+    id,
+    placeholder,
+    isFocused }) => {
+
+    const ref = React.createRef();
+
+    useEffect(() => {
+        isFocused && ref.current.focus()
+    });
+
+    return (<input className="bg-white rounded-md mt-1 px-4 py-2 border focus:border-brand focus:shadow-outline-sm focus:outline-none"
+        ref={ref}
+        type={type}
+        name={name}
+        id={id}
+        placeholder={placeholder}
+        spellCheck="false"
+        aria-describedBy=" "
+    />)
+}
+
+export default Field
---
+++ src/components/library/Textfield/index.js
@@ -0,0 +1,2 @@
+import OutlineField from "./Outline"
+export { OutlineField }
