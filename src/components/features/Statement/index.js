c2a4ac30b344dc6f155da5dd9a86dd0612b207bd
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 05:31:40 -0700

    :tada: Create UniqueValueStatement Component

---
+++ src/components/features/Statement/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const UniqueValueStatement = () => (
+    <p className="font-helvetica font-bold text-6xl text-center text-brand-dark leading-snug">
+        Create Apps In
+        <span className="text-brand"> Minutes</span>
+        <br />
+        Without Code
+    </p>
+)
+
+export default UniqueValueStatement
