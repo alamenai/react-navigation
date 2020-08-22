ba6197b284e773e59e2d90c5ac4b67cb8c03a8ba
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 13:44:26 -0700

    :wastebasket: Remove useless white spaces

--- src/components/features/Logo/index.js
+++ src/components/features/Logo/index.js
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom"
 import { HOME } from "../../../constants/routes"
 
 const Logo = () => (
-    <Link to={HOME.link} className="font-bold text-white text-2xl">
-        <span className="flex flex-col bg-brand w-10 h-10 rounded-full justify-center items-center text-center cursor-pointer">
+    <Link to={HOME.link} className="font-bold text-white text-2xl ">
+        <span className="flex flex-col bg-brand w-10 h-10 rounded-full justify-center items-center text-center cursor-pointer
+        ">
             t
         </span>
     </Link>)
