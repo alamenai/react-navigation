a5c01f2ecb3e301eff205937dac4f1b8ec63db7d
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 18:11:36 -0700

    :tada: Create routes.js for creating and exporting ROUTES

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME = { link: "/" };
+export const FEATURES = { name: "Features", link: "/features" };
+export const PRODUCTS = { name: "Products", link: "/products" };
+export const CUSTOMERS = { name: "Customers", link: "/customers" };
+export const SALES = { name: "Sales", link: "/sales" };
+export const LOGIN = { name: "Login", link: "/login" };
