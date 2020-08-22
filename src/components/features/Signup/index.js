3c1e3f472c18f5beea416765974c864ad30bb607
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 05:32:26 -0700

    :tada: Create Signup Component

---
+++ src/components/features/Signup/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { BrandButtonDefault } from "../../library/Button"
+
+const SignUpButton = () => (
+    <Link to="/signup">
+        <BrandButtonDefault
+            type="button"
+            value="Sign up"
+        />
+    </Link>)
+
+
+export default SignUpButton
