2acfe7fde9d657d552299db430efd9a16f5999b0
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 06:00:12 -0700

    :cyclone: Increase text darkness by changing text-gray-600 to text-gray-700

--- src/components/features/Navigation/Links/Features/Menu/Item/index.js
+++ src/components/features/Navigation/Links/Features/Menu/Item/index.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { Link, } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const Item = ({ name, route }) => {
-    return < Link to={`/features/${route}`} className="text-gray-600 text-base-14 hover:text-brand" > {name}</Link>
-}
+const Item = ({ name, route }) => (
+    < Link to={`/features/${route}`} className="text-gray-700 text-base-14 hover:text-brand" > {name}</Link>)
 
 export default Item
