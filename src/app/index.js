fa18e980c637504750a3561c250a8ddd79d7412e
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 13:39:09 -0700

    :zap: Add HeaderMobile layout for navigation in mobile devices

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,15 @@ import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import Header from "../layouts/Header"
 import { Switcher } from "../components/features/Navigation"
+import HeaderMobile from "../layouts/Header/mobile"
 
 class App extends React.Component {
     render() {
         return (<div>
-            <Router>
+            <Router >
                 <Header />
                 <Switcher />
+                <HeaderMobile forMobile={true} />
             </Router>
         </div>
         )
