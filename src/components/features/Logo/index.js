64f2af790219732412b62ebdf66fcdc3fed864f9
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-22 05:39:18 -0700

    :tada: Create Logo Component

---
+++ src/components/features/Logo/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { HOME } from "../../../constants/routes"
+
+const Logo = () => (
+    <Link to={HOME.link} className="font-bold text-white text-2xl">
+        <span className="flex flex-col bg-brand w-10 h-10 rounded-full justify-center items-center text-center cursor-pointer">
+            t
+        </span>
+    </Link>)
+
+export default Logo
