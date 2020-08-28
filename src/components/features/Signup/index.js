623a3a7285c84494df268b5d3f67d6c5bb7a5d0f
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-27 19:04:08 -0700

    :cyclone: Add mr-5 for aligning the button on the right

--- src/components/features/Signup/index.js
+++ src/components/features/Signup/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { BrandButtonDefault } from "../../library/Button"
 
 const SignUpButton = () => (
-    <Link to="/signup">
+    <Link to="/signup" className="mr-5">
         <BrandButtonDefault
             type="button"
             value="Sign up"
